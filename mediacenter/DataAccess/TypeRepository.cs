23713cfa7ec1bf4a36489c26105aa9845f8cb58d
Thierry Renaud <thierry1.renaud@gmail.com>
2013-02-24 16:32:03 +0100

    Mise en place du style et de l'architecture MVVM

---
+++ mediacenter/DataAccess/TypeRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mediacenter.DataAccess
+{
+    public class TypeRepository
+    {
+        #region Entities
+
+        private MediaCenterDBEntities entities = new MediaCenterDBEntities();
+
+        #endregion
+
+        #region Champs
+
+        readonly List<Type> _types;
+
+        #endregion
+
+        #region Constructeur
+
+        public TypeRepository()
+        {
+            _types = entities.Types.ToList();
+        }
+
+        #endregion
+
+        #region MyRegion
+
+        public List<Type> GetBaseCategories()
+        {
+            return new List<Type>(_types.Where(t => t.TypeParentId == null));
+        }
+
+        #endregion
+    }
+}
